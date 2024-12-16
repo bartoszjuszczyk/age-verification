638ae3b764463dc34476aea53110f0982e7ec5f1
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2024-12-16 04:42:27 +0100

    feat: add configuration provider for age verification

---
+++ Api/Config/ConfigProviderInterface.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * File: ConfigProviderInterface.php
+ *
+ * @author Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
+ * @copyright Copyright (c) 2024.
+ **/
+
+declare(strict_types=1);
+
+namespace Juszczyk\AgeVerification\Api\Config;
+
+interface ConfigProviderInterface
+{
+    public const string XML_PATH_GENERAL_ENABLED = 'juszczyk_age_verification/general/enabled';
+    public const string XML_PATH_GENERAL_VERIFICATION_AGE = 'juszczyk_age_verification/general/verification_age';
+    public const string XML_PATH_GENERAL_COOKIE_TTL = 'juszczyk_age_verification/general/cookie_ttl';
+    public const string XML_PATH_APPEARANCE_HEADING = 'juszczyk_age_verification/appearance/heading';
+    public const string XML_PATH_APPEARANCE_DESCRIPTION = 'juszczyk_age_verification/appearance/description';
+    public const string XML_PATH_APPEARANCE_CONTINUE_BUTTON = 'juszczyk_age_verification/appearance/continue_button';
+    public const string XML_PATH_APPEARANCE_BACK_BUTTON = 'juszczyk_age_verification/appearance/back_button';
+
+    /**
+     * @return bool
+     */
+    public function isModuleEnabled(): bool;
+
+    /**
+     * @return int|null
+     */
+    public function getVerificationAge(): ?int;
+
+    /**
+     * @return string|null
+     */
+    public function getCookieTtl(): ?string;
+
+    /**
+     * @return string|null
+     */
+    public function getHeadingText(): ?string;
+
+    /**
+     * @return string|null
+     */
+    public function getDescriptionText(): ?string;
+
+    /**
+     * @return string|null
+     */
+    public function getContinueButtonText(): ?string;
+
+    /**
+     * @return string|null
+     */
+    public function getBackButtonText(): ?string;
+
+    /**
+     * @return string
+     */
+    public function getDefaultCookiePath(): string;
+}
---
+++ Model/Config/ConfigProvider.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * File: ConfigProvider.php
+ *
+ * @author Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
+ * @copyright Copyright (c) 2024.
+ **/
+
+declare(strict_types=1);
+
+namespace Juszczyk\AgeVerification\Model\Config;
+
+use Juszczyk\AgeVerification\Api\Config\ConfigProviderInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Session\Config;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+class ConfigProvider implements ConfigProviderInterface
+{
+    public function __construct(
+        protected readonly ScopeConfigInterface $scopeConfig,
+        protected readonly StoreManagerInterface $storeManager
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isModuleEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_GENERAL_ENABLED,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getVerificationAge(): ?int
+    {
+        $value = $this->scopeConfig->getValue(
+            self::XML_PATH_GENERAL_VERIFICATION_AGE,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+        if ($value === null) {
+            return null;
+        }
+        return (int) $value;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCookieTtl(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_GENERAL_COOKIE_TTL,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getHeadingText(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_APPEARANCE_HEADING,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDescriptionText(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_APPEARANCE_DESCRIPTION,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getContinueButtonText(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_APPEARANCE_CONTINUE_BUTTON,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBackButtonText(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_APPEARANCE_BACK_BUTTON,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDefaultCookiePath(): string
+    {
+        return $this->scopeConfig->getValue(
+            Config::XML_PATH_COOKIE_PATH,
+            ScopeInterface::SCOPE_STORE,
+            $this->storeManager->getStore()->getId()
+        );
+    }
+
+}
