be1004ff82244d6dad84ef8b34b223da0276f41f
Bartosz Juszczyk <b.juszczyk@bjuszczyk.pl>
2024-12-17 23:39:46 +0100

    fix: improve date of birth validation logic

--- view/frontend/web/js/age-verification.js
+++ view/frontend/web/js/age-verification.js
@@ -1,20 +1,35 @@
 define([
     'jquery',
     'uiComponent',
-    'mage/url'
+    'mage/url',
+    'jquery/validate'
 ], function ($, Component, url) {
     "use strict";
     return Component.extend({
         initialize: function (config, data) {
             this.subscribeContinueButton();
             this.subscribeBackButton();
+            this.addValidationRule();
             this.cookieTtl = parseInt(config.cookie_ttl);
             this.cookiePath = config.cookie_path;
             this.cookieDomain = config.cookie_domain;
         },
+        addValidationRule: function () {
+            $.validator.addMethod("ageverification", function (value, element) {
+                let inputDate = Date.parse(value);
+                let now = new Date();
+                let verificationThreshold = now.setFullYear(now.getFullYear() - 18);
+                return inputDate <= verificationThreshold;
+            }, "Your age does not complain with terms.");
+        },
         subscribeContinueButton: function () {
             let self = this;
             $("button#j_agever_continue_btn").on('click', function () {
+                let dobForm = $("#j_ageverification_dob_form");
+                dobForm.validate();
+                if (!dobForm.valid()) {
+                    return false;
+                }
                 self.saveCookie();
                 self.hideModal();
             });
@@ -34,6 +49,16 @@ define([
         },
         hideModal: function () {
             $("#j_ageverification_modal").hide();
+        },
+        verifyDobInput: function () {
+            let dobInputSelector = $("input#j_ageverification_dob_input");
+            if (!(dobInputSelector.length)) {
+                return false;
+            }
+            let inputDate = Date.parse(dobInputSelector.val());
+            let now = new Date();
+            let verificationThreshold = now.setFullYear(now.getFullYear() - 18);
+            return inputDate <= verificationThreshold;
         }
     });
 });
